eaed8dfe153d8df1f1cba83a9ac7d493535dc211
anijit-mlsa <anijit.mlsa@gmail.com>
2024-06-15 13:40:47 +0000

    initial commit

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,27 +16,27 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "../images/mountains.jpg";
 
-const imageAltText = "purple and blue abstract background";
+const imageAltText = "A picture depicting mountains representing calmness.";
 
 /**
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I am an aspiring web developer always open to learning new things.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Web Developer",
+  "Problem Solving",
+  "Analytical Skills",
+  "SEO",
+  "Frameworks and Libraries",
+  "Backend Skills",
 ];
 
 /**
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I love to explore new opportunities. A quote I live by - 'Rest at the end and not in the middle' -Kobe Bryant. ";
 
 const About = () => {
   return (
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,9 +19,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/boat.jpg";
 
-const imageAltText = "desktop with books and laptop";
+const imageAltText = "A picture depicting boat beside the beach.";
 
 /**
  * Project list
